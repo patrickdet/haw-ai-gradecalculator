be03dcfc19c7728ad772b70acfdc551cfd14bbd7
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-07-23 18:42:33 +0200

    Initial commit

---
+++ site.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+  $("li > a").click(function() {
+    if ($(this).hasClass("selected")) {
+      $(this).toggleClass("selected");
+    } else {
+      $(this).parent().parent().children().each(function() {
+        $(this).children().removeClass("selected");
+      });
+      $(this).addClass("selected");
+    }
+    calculate();
+    return false;
+  });
+
+  function calculate() {
+    var totalGradeWeight = 0.0;
+    var points = 0.0;
+    var selected = $(".selected");
+    selected.each(function() {
+      var parent = $(this).parent().parent().parent();
+      if (parent.data("grade-multiplier") === undefined) {
+        points += parent.data("credit-points") * 15;
+      } else {
+        points += parseFloat($(this).html()) * parent.data("credit-points");
+      }
+      totalGradeWeight += parent.data("credit-points");
+    });
+    if (totalGradeWeight == 0) {
+      $("#grade").text(0);
+    } else {
+      $("#grade").text(Math.round((points/totalGradeWeight)*10)/10);
+    }
+    $("#calculated-points").text("(" + points + " Punkte)");
+  }
+
+  $("#calculated-points").click(function() {
+    $("#infobox").toggle();
+    return false;
+  });
+});
+
